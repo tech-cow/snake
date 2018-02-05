aa0e9d3dec6a8b12288f7b5d1910fc2d346e29c9
yuzhoujr <junior147147@yahoo.com>
2018-02-05 15:44:48 -0800

    failed to really classify this...

--- snake.py
+++ snake.py
@@ -3,92 +3,70 @@ import curses
 
 direction_option = [curses.KEY_DOWN, curses.KEY_UP, curses.KEY_LEFT, curses.KEY_RIGHT]
 
-screen_h = screen_w = 0
+"""Initialize Terminal Screen.
 
-def init_screen(screen_h, screen_w):
-    """Initialize Terminal Screen.
+:api initscr(): Initialize the library.
+:api curs_set(): Set the cursor state to invisible
+:api getmaxyx(): return a tuple of screen height and screen Width
+:api newwin(): create a new window with 4 corner coordinate.
+:api keypad(): accept keypad input
+:api timeout(): refresh the screen every input ms.
+"""
+init = curses.initscr()
+curses.curs_set(0)
+screen_h, screen_w = init.getmaxyx()
+window = curses.newwin(screen_h, screen_w, 0, 0)
+window.keypad(1)
+window.timeout(100)
 
-    :param screen_h: Height of the Terminal Screen.
-    :param screen_w: Width of the Terminal Screen.
+"""Initialize Snake Initial Position and Direction"""
+snk_x = screen_w/4
+snk_y = screen_h/2
+snake = [
+    [snk_y, snk_x],
+    [snk_y, snk_x-1],
+    [snk_y, snk_x-2]
+]
 
-    :api initscr(): Initialize the library.
-    :api curs_set(): Set the cursor state to invisible
-    :api getmaxyx(): return a tuple of screen height and screen Width
-    :api newwin(): create a new window with 4 corner coordinate.
-    :api keypad(): accept keypad input
-    :api timeout(): refresh the screen every input ms.
-    """
-    init = window.initscr()
-    curses.curs_set(0)
-    screen_h, screen_w = init.getmaxyx()
-    window = window.newwin(screen_h, screen_w, 0, 0)
-    window.keypad(1)
-    window.timeout(100)
+"""Initialize Food Initial Position"""
+food = [screen_h/2, screen_w/2]
+window.addch(food[0], food[1], curses.ACS_DIAMOND)
 
-def init_snake(screen_h, screen_w):
-    # Snake initial position
-    # Snake Bodyparts
-    snk_x = screen_w/4
-    snk_y = screen_h/2
-    snake = [
-        [snk_y, snk_x],
-        [snk_y, snk_x-1],
-        [snk_y, snk_x-2]
-    ]
-    # Snake initial moving direction
-    direction = window.KEY_RIGHT
+key = curses.KEY_RIGHT
 
-def init_food():
-    # Food starting place
-    food = [sh/2, sw/2]
-    # Add food to the screen
-    w.addch(food[0], food[1], curses.ACS_DIAMOND)
-# While loop isn't the most optimal decision here..
+"""Logic"""
 while True:
-    # Variable to store our next key input, return -1 if no inputs
-    next_key = w.getch()
-    # Edge to set the next key.
-    if next_key is not -1 and next_key in key_option:
-        direction = next_key
+    next_key = window.getch()
+    key = key if next_key == -1 else next_key
 
-    # Ternimating condition
-    if snake[0][0] in [0, sh] or snake[0][1]  in [0, sw] or snake[0] in snake[1:]:
-        window.endwin()
+    if snake[0][0] in [0, screen_h] or snake[0][1]  in [0, screen_w] or snake[0] in snake[1:]:
+        curses.endwin()
         quit()
 
     new_head = [snake[0][0], snake[0][1]]
 
-    # This should be improving
-    if direction is curses.KEY_DOWN:
+    if key == curses.KEY_DOWN:
         new_head[0] += 1
-    if direction is curses.KEY_UP:
+    if key == curses.KEY_UP:
         new_head[0] -= 1
-    if direction is curses.KEY_LEFT:
+    if key == curses.KEY_LEFT:
         new_head[1] -= 1
-    if direction is curses.KEY_RIGHT:
+    if key == curses.KEY_RIGHT:
         new_head[1] += 1
 
     snake.insert(0, new_head)
 
-    if snake[0] is food:
+    if snake[0] == food:
         food = None
         while food is None:
             nf = [
-                random.randint(1, sh-1),
-                random.randint(1, sw-1)
+                random.randint(1, screen_h-1),
+                random.randint(1, screen_w-1)
             ]
             food = nf if nf not in snake else None
-        w.addch(food[0], food[1], curses.ACS_DIAMOND)
+        window.addch(food[0], food[1], curses.ACS_CKBOARD)
     else:
         tail = snake.pop()
-        w.addch(tail[0], tail[1], ' ')
+        window.addch(tail[0], tail[1], ' ')
 
-    w.addch(snake[0][0], snake[0][1], curses.ACS_CKBOARD)
-
-
-def main():
-    init_screen()
-    init_snake()
-    init_food()
-
-main()
+    window.addch(snake[0][0], snake[0][1], curses.ACS_CKBOARD)
